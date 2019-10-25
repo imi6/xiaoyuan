bde4b93f303226d93202c97cff053519a402ba21
imi6 <56383294+imi6@users.noreply.github.com>
2019-10-25 19:47:19 +0800

    Update ip.js

--- ip.js
+++ ip.js
@@ -7,10 +7,12 @@ $httpClient.get(weaapi, function(error, response, data){
     } else {
         var obj = JSON.parse(data);
         console.log(obj);
-        var dizhi = "所在城市： " + obj.regionName;
+        var guojia = "所在国家： " + obj.country;
+        var shenfen = "所在省份： " + obj.regionName;
+        var chengshi = "所在城市： " + obj.city;
         var szip = "所在IP： " + obj.query;
-        let wmation = [dizhi,szip];
-        $notification.post(wmation[0], wmation[1]);
+        let wmation = [guojia,shenfen,chengshi,szip];
+        $notification.post(wmation[0], wmation[1], wmation[2], wmation[3]);
         $done();
     }
 }
